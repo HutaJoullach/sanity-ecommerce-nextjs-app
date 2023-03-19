aa2b6535a580f35e2605197da1ec0c205833a34d
HutaJoullach <hutajoullach@gmail.com>
2023-03-19 18:08:20 +0900

    created boiler page for home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,21 @@
 import styles from "@/styles/Home.module.css";
 
 const Home = () => {
-  return <div className={styles.container}>hey</div>;
+  return (
+    <>
+      {/* HeroBanner */}
+      <div className="products-heading">
+        <h2>Best Selling Products</h2>
+        <p>Speakers of many variations</p>
+      </div>
+      {/* <div className="products-container">
+        {products?.map((product) => (
+          <Product key={product._id} product={product} />
+        ))}
+      </div> */}
+      {/* FooterBanner */}
+    </>
+  );
 };
 
 export default Home;
