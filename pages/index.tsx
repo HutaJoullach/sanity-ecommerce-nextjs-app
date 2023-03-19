6bc39fb1c02ce2d449c3a1d9241cb050f5721fa5
HutaJoullach <hutajoullach@gmail.com>
2023-03-19 18:36:05 +0900

    added client.ts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
 import styles from "@/styles/Home.module.css";
 
+// import { client } from "../lib/client";
+import { HeroBanner } from "../components";
+
 const Home = () => {
   return (
     <>
-      {/* HeroBanner */}
+      {/* <HeroBanner heroBanner={bannerData.length && bannerData[0]} /> */}
       <div className="products-heading">
         <h2>Best Selling Products</h2>
         <p>Speakers of many variations</p>
